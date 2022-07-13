c05ecbecaa0f3314922cab3e5fd61553299e4707
Camaechi <amaechi@yellowcard.io>
2022-07-13 20:24:21 +0400

    init

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input
+      :value="value.toUpperCase()"
+      @input="updateValue"
+      v-bind="$attrs"
+      v-on="listeners"
+    />
+  </div>
+</template>
+
+<script>
+import { formFieldMixin } from "../mixins/formFieldMixin";
+export default {
+  mixins: [formFieldMixin],
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: this.updateValue,
+      };
+    },
+  },
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,154 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="main">
+    <div id="capture">
+      <img width="600px" alt="Ticket" :src="img" />
+      <h2 v-show="showCode" class="bosco">#{{ code }}</h2>
+      <br />
+      <p v-show="false" class="bosco-details">
+        {{ event.tableFor }} -
+        <span
+          ><strong>{{ event.name.toUpperCase() }}</strong></span
+        >
+      </p>
+    </div>
+    <h1>GENERATE E-TICKET</h1>
+    <form @submit.prevent="createEvent">
+      <BaseInput
+        v-model="event.name"
+        type="text"
+        placeholder="Name"
+        class="field"
+        :class="{ error: $v.event.name.$error }"
+        @blur="$v.event.name.$touch()"
+      />
+      <BaseSelect
+        :options="tableList"
+        v-model="event.tableFor"
+        :class="{ error: $v.event.tableFor.$error }"
+        @blur="$v.event.tableFor.$touch()"
+      />
+      <BaseButton
+        type="submit"
+        buttonClass="-fill-gradient"
+        :disabled="$v.$anyError"
+        >Generate</BaseButton
+      >
+      <p v-if="$v.$anyError" class="errorMessage">
+        Please fill out the required field(s).
+      </p>
+    </form>
+
+    <div id="ticket"></div>
   </div>
 </template>
 
 <script>
+import ticketCode from "ticket-code";
+import { required } from "vuelidate/lib/validators";
+import BaseInput from "../components/BaseInput.vue";
+import BaseButton from "../components/BaseButton.vue";
+import BaseSelect from "../components/BaseSelect.vue";
+import html2canvas from "html2canvas";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      codeLength: 8,
+      checksumSeed: 5,
+      checksumIdx: 7,
+      code: this.generateCode(),
+      showCode: false,
+      img: require("@/assets/dellusso.jpg"),
+      tableList: [
+        "REGULAR TABLE LADIES",
+        "REGULAR TABLE MEN",
+        "VIP TABLE FOR #THREE",
+        "VIP TABLE FOR #FIVE",
+        "VIP TABLE FOR #TEN",
+        "VVIP",
+      ],
+      event: { name: "", tableFor: "" },
+    };
+  },
+  validations: {
+    event: {
+      name: { required },
+      tableFor: { required },
+    },
+  },
+  watch: {
+    "event.tableFor": function (table) {
+      if (table === "VIP TABLE FOR #TEN") {
+        this.img = require("@/assets/ten.jpg");
+        this.showCode = true;
+        return;
+      }
+      if (table === "VIP TABLE FOR #FIVE") {
+        this.img = require("@/assets/five.jpg");
+        this.showCode = true;
+        return;
+      }
+      if (table === "VIP TABLE FOR #THREE") {
+        this.img = require("@/assets/three.jpg");
+        this.showCode = true;
+        return;
+      }
+      if (table === "VVIP") {
+        this.img = require("@/assets/vvip.jpg");
+        this.showCode = true;
+        return;
+      }
+      if (table === "REGULAR TABLE MEN") {
+        this.img = require("@/assets/300.jpg");
+        this.showCode = true;
+        return;
+      }
+      if (table === "REGULAR TABLE LADIES") {
+        this.img = require("@/assets/200.jpg");
+        this.showCode = true;
+        return;
+      }
+      this.showCode = false;
+      return;
+    },
+  },
+  methods: {
+    generateCode() {
+      return ticketCode.generate(
+        this.checksumSeed,
+        this.codeLength,
+        this.checksumIdx
+      );
+    },
+
+    createEvent() {
+      this.generateCode();
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
+        html2canvas(document.querySelector("#capture")).then((canvas) => {
+          document.querySelector("#ticket").appendChild(canvas);
+        });
+        this.code = this.generateCode();
+        this.resetFormData();
+      }
+    },
+    resetFormData() {
+      this.event = { name: "", tableFor: "" };
+    },
+  },
+  components: {
+    BaseInput,
+    BaseSelect,
+    BaseButton,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.main {
+  width: 600px;
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -54,5 +162,31 @@ li {
 }
 a {
   color: #42b983;
+}
+.bosco {
+  padding: 7px;
+  position: relative;
+  font-size: 18px;
+  bottom: 350px;
+  color: white;
+  font-weight: 900;
+  background-color: #c00;
+  border-radius: 10px;
+  width: 140px;
+  float: right;
+  right: 200px;
+}
+.bosco-details {
+  margin: 0 auto;
+  padding: 10px;
+  position: absolute;
+  font-size: 14px;
+  color: #fff;
+  top: 376px;
+  font-weight: bold;
+  float: left;
+}
+#ticket {
+  margin-top: 100px;
 }
 </style>
